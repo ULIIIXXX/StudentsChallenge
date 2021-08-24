709d7960b8e644fead2612835feffe6711568850
ULIIIXXX <51976432+ULIIIXXX@users.noreply.github.com>
2021-08-24 10:06:28 -0500

    new challenge

--- src/main/java/com/ibm/cursos/controller/CursosRestController.java
+++ src/main/java/com/ibm/cursos/controller/CursosRestController.java
@@ -3,14 +3,20 @@ package com.ibm.cursos.controller;
 import java.util.ArrayList;
 import java.util.List;
 import javax.annotation.PostConstruct;
+import javax.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import com.ibm.cursos.configs.Mapping;
 import com.ibm.cursos.models.Curso;
 
+@RestController
 @RequestMapping(Mapping.CURSOS)
 public class CursosRestController {
   
@@ -28,6 +34,22 @@ public class CursosRestController {
   @GetMapping(value = "/listar", produces = MediaType.APPLICATION_JSON_VALUE)
   public ResponseEntity<List<Curso>> listarCursos() {
     return new ResponseEntity<>(cursos,HttpStatus.OK);
+  }
+  
+  @GetMapping(value="/listar/{nombre}")
+  public ResponseEntity<Curso> buscarCurso(@PathVariable("nombre") String nombre){   
+    Curso curso = new Curso();
+    for(Curso c : cursos) {
+      if(c.getNombre().contains(nombre)) {
+        curso = c;
+      }
+    }
+    return new ResponseEntity<>(curso,HttpStatus.OK);
+  }
+  
+  @PostMapping(value ="/crear", produces = MediaType.APPLICATION_JSON_VALUE)
+  public String crearCurso(@RequestBody @Valid Curso curso) {
+    return "";
   }
   
 }
